79fc3a6d1140d0970062cc5fcb672846682eeef6
Scott Chiang <scott.chiang1@gmail.com>
2012-12-01 16:11:33 -0800

    fixed spelling error on sessions controller, added user model, added sessions routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :email, :name
+  
+  has_many :meals
+  
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.image_url = auth.info.image
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 ProjectFood::Application.routes.draw do
 
+match 'auth/:provider/callback', to: 'sessions#create'
+match 'auth/failure', to: redirect('/')
+match 'signout', to: 'sessions#destroy', as: 'signout'
+
 resources :meals
+resources :sessions
 
 root :to => 'meals#index'
 
