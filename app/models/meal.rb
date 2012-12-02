c2d751586e65f366bab78b7f562770a12636ad5d
Brian Lee <brian.g.lee89@gmail.com>
2012-12-01 20:40:24 -0800

    include validation for price to have $ as an option

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,9 +3,12 @@ class Meal < ActiveRecord::Base
   validates_presence_of :category, :description, :photo, :price, :user_id
   validates :portion, numericality: true
   validates :category, :inclusion => { :in => %w(Breakfast Entree Dessert) }
+  validate :correct_price?
   belongs_to :user
 
   def correct_price? #validation for 2 decimals in price max?
-    # if self.price =~ /d{1,}
+    if self.price !=~ /\${,1}\d{1,}.\d{1,2}/
+      errors.add(:price, "must be in the form $0.00")
+    end
   end
 end
